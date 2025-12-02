97d566422348b83df412d803f8d4ed65336d6d84
sofiarashid <527srashid@frhsd.com>
2025-12-02 11:55:58 -0500

    did 2 img + switch

---
+++ 8ball.js
@@ -0,0 +1,66 @@
+function shakeMagic8Ball() {
+    const question = prompt("What is your question?");
+    if(question===null){
+        document.getElementById('response-text').innerText = "Please ask a question!"
+        document.getElementById('response-text').classList = 'lead text-warning'
+        document.getElementById('response-img').src = 'unhappyFace.jpg'
+    }
+
+    if (!question.trim()) {
+        alert('Please ask a valid question.')
+    }
+
+    const randomNumber = Math.floor(Math.random()*8);
+    let answer, image, color;
+
+    switch(randomNumber) {
+        case 0:
+            answer = "It is certain";
+            image='0-image.jpg';
+            color='text-success';
+            break;
+        case 1:
+            answer = "It is decidedly so";
+            image='1-image.png';
+            color='text-danger';
+            break;
+        case 2:
+            answer = "Reply hazy, try again";
+            image='2-image.jpg';
+            color='text-warning';
+            break;
+        case 3:
+            answer = "Cannot predict now";
+            image='3-image.jpg';
+            color='text-success';
+            break;
+        case 4:
+            answer = "Do not count on it";
+            image='4-image.jpg';
+            color='text-info';
+            break;
+        case 5:
+            answer = "My sources say no";
+            image='5-image.jpg';
+            color='text-danger';
+            break;
+        case 6:
+            answer = "Outlook not so good";
+            image='6-image.jpg';
+            color='text-success';
+            break;
+        case 7:
+            answer = "Signs point to yes";
+            image='7-image.jpg';
+            color='text-danger';
+            break;
+        default:
+            answer = "Error";
+            image='default-image.jpg';
+            color='text-secondary';
+    }
+    
+    document.getElementById('response-text').innerText = answer
+    document.getElementById('response-text').classList = `${color}`
+    document.getElementById('response-img').src = image
+}
