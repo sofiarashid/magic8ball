23d12c9610eefce200af5a89628601edb9eb83c8
sofiarashid <527srashid@frhsd.com>
2025-12-05 11:54:47 -0500

    fixed img, did js, finished css

--- 8ball.js
+++ 8ball.js
@@ -3,7 +3,7 @@ function shakeMagic8Ball() {
     if(question===null){
         document.getElementById('response-text').innerText = "Please ask a question!"
         document.getElementById('response-text').classList = 'lead text-warning'
-        document.getElementById('response-img').src = 'unhappyFace.jpg'
+        document.getElementById('response-img').src = 'unhappyFace.png'
     }
 
     if (!question.trim()) {
@@ -16,7 +16,7 @@ function shakeMagic8Ball() {
     switch(randomNumber) {
         case 0:
             answer = "It is certain";
-            image='0-image.jpg';
+            image='0-image.png';
             color='text-success';
             break;
         case 1:
@@ -26,37 +26,37 @@ function shakeMagic8Ball() {
             break;
         case 2:
             answer = "Reply hazy, try again";
-            image='2-image.jpg';
+            image='2-image.png';
             color='text-warning';
             break;
         case 3:
             answer = "Cannot predict now";
-            image='3-image.jpg';
+            image='3-image.png';
             color='text-success';
             break;
         case 4:
             answer = "Do not count on it";
-            image='4-image.jpg';
+            image='4-image.png';
             color='text-info';
             break;
         case 5:
             answer = "My sources say no";
-            image='5-image.jpg';
+            image='5-image.png';
             color='text-danger';
             break;
         case 6:
             answer = "Outlook not so good";
-            image='6-image.jpg';
+            image='6-image.png';
             color='text-success';
             break;
         case 7:
             answer = "Signs point to yes";
-            image='7-image.jpg';
+            image='7-image.png';
             color='text-danger';
             break;
         default:
             answer = "Error";
-            image='8-image.jpg';
+            image='8-image.png';
             color='text-secondary';
     }
     
